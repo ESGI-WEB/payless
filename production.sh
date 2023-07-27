72969f313a2ef3750ad0be83a366ad18c45495bc
Nicolas W <nwadoux@myges.fr>
2023-07-27 11:02:44 +0200

    wip mep

---
+++ production.sh
@@ -0,0 +1,19 @@
+# Pull repository
+git pull
+
+# create networks if they doesn't exist
+docker network create fake-psp
+docker network create payless-network
+
+# launch dockers
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build
+
+# launch migrations
+docker exec payless_api_1 node migrate.js
+
+# launch nginx for frontend
+docker build -t nicolaswadoux/payless-backoffice ./payless-backoffice
+docker stop payless-backoffice-prod
+docker run --name payless-backoffice-prod -d -p 80:80 nicolaswadoux/payless-backoffice
+
+
